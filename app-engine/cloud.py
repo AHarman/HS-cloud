a24c52115a701266541a01e938d5208416aa76b9
AHarman <alexanderjharman@gmail.com>
2016-01-07 22:56:32 +0000

    We now have working uploads!

--- app-engine/cloud.py
+++ app-engine/cloud.py
@@ -18,7 +18,7 @@ MAIN_PAGE_HTML = """\
     <form method="POST" action={0} enctype="multipart/form-data">
       <label for="title">Upload screenshots </label>
       <input type="file" name="screenshot" id="files" multiple/>
-      <button type="button" onclick="doSomething()">Enter</button>
+      <button type="button" onclick="upload()">Upload</button>
     </form>
     <br/><br/>
     <div id="uploadStatus"></div>
@@ -47,77 +47,76 @@ class UploadHandler(blobstore_handlers.BlobstoreUploadHandler, webapp2.RequestHa
 		try:
 			user = users.get_current_user()
 			uploads = self.get_uploads()
-			uploads = self.reorderImages(uploads);
-			
+			print uploads[0].filename
 			user_upload = UserScreenshot(
 				user=user.user_id(),
 				blob_key=uploads[0].key(),
-				number=0)
+				filename=uploads[0].filename)
 			user_upload.put()
-			previous = user_upload.key
-			for i in range(1, len(uploads)):
-				upload = uploads[i]
-				user_upload = UserScreenshot(
-					user=user.user_id(),
-					blob_key=upload.key(),
-					number=i,
-					parent=previous)
-				user_upload.put()
-				previous=user_upload.key
+			# previous = user_upload.key
+			# for i in range(1, len(uploads)):
+			# 	upload = uploads[i]
+			# 	user_upload = UserScreenshot(
+			# 		user=user.user_id(),
+			# 		blob_key=upload.key(),
+			# 		number=i,
+			# 		parent=previous)
+			# 	user_upload.put()
+			# 	previous=user_upload.key
 			print "Over here"
 
 			self.response.write("uploaded")
-
-			#self.redirect("/result/hereissometext")
 
 		except Exception as e:
 			print e
 			self.error(500)
 
-	def reorderImages(self, blobs):
-		blobs.sort(key=lambda x: x.filename)
-		sortedBlobs = []
-		i = 0
-		while i < len(blobs):
-			blob = blobs[i]
-			if blob.filename[-6:-5] == " ":
-				sortedBlobs.append(blobs[i+1])
-				i += 1
-			sortedBlobs.append(blob)
-			i += 1
-		return sortedBlobs
-
 class ResultHandler(webapp2.RequestHandler):
-	def get(self, thething):
+	def get(self):
 		user = users.get_current_user()
 		if not user:
 			self.redirect(users.create_login_url(self.request.uri))
 		else:
 			query = UserScreenshot.query(UserScreenshot.user == user.user_id())
-
 			res = query.fetch(None)
+
 			length = len(res)
-			p = ScreenshotParser()
-			cards = []
+			if length == 0:
+				self.redirect("/")
 			if length > 0:
+				res = self.reorderImages(res)
+				p = ScreenshotParser()
+				cards = 0
+			
 				for entry in res:
 					blob_key = entry.blob_key
 					blobstore.get(blob_key)
 					reader = blobstore.BlobReader(blob_key)
-					images = Image.open(reader)
-
-					cards += p.getCardsFromImages([images])
+					image = Image.open(reader)
+					cards += p.numOfCardsInScreenshot(image)
+					#cards += p.getCardsFromImages([images])
 					entry.key.delete()
 					blobstore.delete(blob_key)
-			
-			self.response.write("You have " + str(len(cards)) + " cards")
 
+			self.response.write("You have " + str(cards) + " cards")
 
+	def reorderImages(self, userScreenshots):
+		userScreenshots.sort(key=lambda x: x.filename)
+		sortedUserScreenshots = []
+		i = 0
+		while i < len(userScreenshots):
+			blob = userScreenshots[i]
+			if blob.filename[-6:-5] == " ":
+				sortedUserScreenshots.append(userScreenshots[i+1])
+				i += 1
+			sortedUserScreenshots.append(blob)
+			i += 1
+		return sortedUserScreenshots
 
 class UserScreenshot(ndb.Model):
 	user = ndb.StringProperty(indexed=True)
 	blob_key = ndb.BlobKeyProperty(indexed=False)
-	number = ndb.IntegerProperty(indexed=False)
+	filename = ndb.StringProperty(indexed=True)
 
 class UserCollection(ndb.Model):
 	user = ndb.StringProperty()
@@ -126,5 +125,5 @@ class UserCollection(ndb.Model):
 app = webapp2.WSGIApplication([
 	('/', MainPage),
 	('/upload', UploadHandler),
-	('/result/([^/]+)?', ResultHandler),
+	('/result', ResultHandler),
 ], debug=True)
---
+++ app-engine/scripts/upload.js
@@ -0,0 +1,87 @@
+imagesUploaded = 0; 
+imagesToUpload = 0;
+
+// Number of uploads to do at once
+// Set to 1 so we can easily reattempt uploads
+simulUploadLimit = 1
+
+function getLinks()
+{
+	linkTags = document.getElementsByClassName("uploadLink");
+	links = [];
+	for (i = 0; i < linkTags.length; i++)
+	{
+		links.push(linkTags[i].getAttribute("upload"));
+	}
+	return links
+}
+
+function successMessage()
+{
+	console.log("All images uploaded");
+	document.getElementById("uploadStatus").innerHTML = "All images uploaded";
+	window.location.href="/result"
+}
+
+function updateUploadStatus()
+{
+	console.log(imagesUploaded + " uploaded");
+	document.getElementById("uploadStatus").innerHTML = "Uploaded " + imagesUploaded + " images of " + imagesToUpload;
+}
+
+function transferComplete(evt)
+{
+	console.log("success");
+	imagesUploaded += 1;
+	if (imagesUploaded == imagesToUpload)
+	{
+		successMessage();
+	}
+	else if (imagesUploaded % simulUploadLimit == 0)
+	{
+		updateUploadStatus(imagesUploaded, imagesToUpload);
+		nUploads(simulUploadLimit);
+	}
+	else
+	{
+		updateUploadStatus(imagesUploaded, imagesToUpload);
+	}
+}
+
+function transferFailed(evt) {
+	console.log("File did not upload, retrying file");
+	nUploads(1);
+}
+
+
+function nUploads(n)
+{
+	var file = document.getElementById('files');
+	links = getLinks();
+	cap = Math.min(imagesUploaded + n, imagesToUpload)
+	var xhrs = []
+	for (var i = imagesUploaded; i < cap; i++)
+	{
+		var formData = new FormData();
+		formData.append("screenshot", file.files[i]);
+		formData.append("index", i + imagesUploaded);
+		var xhr = new XMLHttpRequest();
+		xhrs.push(xhr);
+		xhr.addEventListener("load", transferComplete);
+		xhr.addEventListener("error", transferFailed);
+		xhr.open('POST', links[i], true);
+		xhr.send(formData);
+	}
+}
+
+function upload()
+{
+	var file = document.getElementById('files');
+
+	if(file.files.length)
+	{
+		imagesToUpload = file.files.length;
+		updateUploadStatus()
+		nUploads(simulUploadLimit);
+	}
+}
